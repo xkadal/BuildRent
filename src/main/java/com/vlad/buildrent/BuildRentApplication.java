40017ded6dbb8af0b5a68ad0e26bab25ebdc0d36
xkadal <01-24-291.stud@vntu.vn.ua>
2026-05-05 16:53:48 +0300

    fix

--- src/main/java/com/vlad/buildrent/BuildRentApplication.java
+++ src/main/java/com/vlad/buildrent/BuildRentApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuildrentApplication {
+public class BuildRentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BuildrentApplication.class, args);
+        SpringApplication.run(BuildRentApplication.class, args);
     }
 }
